ce470b7ac26281c04abfc6faff271f59a891e067
jeremie.db <jeremie.db@evovest.com>
2022-03-18 12:03:30 -0400

    startup test

--- bin/main.jl
+++ bin/main.jl
@@ -356,6 +356,7 @@ route("/") do
 end
 
 # up(9000; async = false, server = Stipple.bootstrap())
-up(9000; async = false)
+# up(9000; async = false)
+Stipple.Genie.startup(8000, "0.0.0.0", async = false)
 
 # down()
